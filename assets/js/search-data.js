624bf7b6d090d311679dc91e257040a538eb8354
alcunha <fagner.cunha99@gmail.com>
2025-07-09 03:26:56 +0000

    Deploying to gh-pages from @ alcunha/alcunha.github.io@d43954d11f9a67b0cea888ad7c2ee40f304b2855 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
+        },{id: "nav-code",
+          title: "Code",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
